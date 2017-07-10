69b836f67d744576ebb200108406473868131397
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-07-10 22:31:43 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,13 @@
+MeetupProxy
+=========================
+
+A meetup service proxy of sorts - enables us to get the reponse from meetup
+from the filesystem if present
+Features
+--------
+
+* PSR-4 autoloading compliant structure
+* Unit-Testing with PHPUnit
+
+author: Shaun Hare
+License MIT -see License
---
+++ src/ServiceProxy.php
@@ -0,0 +1,17 @@
+<?php namespace ShaunHare\MeetupProxy;
+
+/**
+*  A Meetup Service Proxy
+*
+*  A meetup service proxy of sorts - enables us to get the reponse from meetup
+*  from the filesystem if present
+*
+*  @author Shaun Hare
+*/
+class ServiceProxy{
+
+   /**  @var string storagePath*/
+   private $storagePath = '';
+ 
+  
+}
