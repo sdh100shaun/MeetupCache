b06173d965e1e5dd445ccdb97002fb1a3b202289
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-07-11 21:06:15 +0100

    updated travis config with phpcs path

--- src/ServiceProxy.php
+++ src/ServiceProxy.php
@@ -4,25 +4,29 @@ use DMS\Service\Meetup\MeetupKeyAuthClient;
 use DMS\Service\Meetup\Response\SingleResultResponse;
 
 /**
-*  A Meetup Service Proxy
-*
-*  A meetup service proxy of sorts - enables us to get the response from meetup
-*  or from the filesystem if present
-*
-*  @author Shaun Hare
-*/
-class ServiceProxy{
+ *  A Meetup Service Proxy
+ *
+ *  A meetup service proxy of sorts - enables us to get the response from meetup
+ *  or from the filesystem if present
+ *
+ * @author Shaun Hare
+ */
+class ServiceProxy
+{
     
     /**
      * @var MeetupKeyAuthClient
      */
     private $client;
     
-    /**  @var string storagePath*/
+    /**
+     * @var string storagePath
+     */
     private $storagePath = '';
     
     /**
      * ServiceProxy constructor.
+     *
      * @param MeetupKeyAuthClient $client
      */
     public function __construct(MeetupKeyAuthClient $client)
@@ -32,9 +36,5 @@ class ServiceProxy{
     
     public function __call($name, $arguments)
     {
-       
     }
-    
-    
-    
 }
